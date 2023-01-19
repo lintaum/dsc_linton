1e175e648aa2f7f792b01784ef7df8c59be23c24
lintaum <lintaum@gmail.com>
2023-01-19 13:16:34 -0300

    Otimização do classificador de ativos, permitindo que o mesmo trabalhe em paralelol

--- fpga_proj/rtl/classificar_ativo.v
+++ fpga_proj/rtl/classificar_ativo.v
@@ -1,7 +1,7 @@
 //==================================================================================================
 //  Filename      : classificar_ativo.v
 //  Created On    : 2022-08-30 09:59:30
-//  Last Modified : 2022-11-22 10:17:28
+//  Last Modified : 2023-01-19 11:14:26
 //  Revision      : 
 //  Author        : Linton Esteves
 //  Company       : UFBA
@@ -24,25 +24,31 @@ module classificar_ativo
 			input [NUM_NA-1:0] na_ativo_in,
 			input [NUM_NA*CRITERIO_WIDTH-1:0] na_criterio_in,
 			output reg ca_pronto_o,
-			output reg [CRITERIO_WIDTH-1:0] ca_criterio_geral_out
+			output [CRITERIO_WIDTH-1:0] ca_criterio_geral_out
 			
 		);
 //*******************************************************
 //Internal
 //*******************************************************
 //Local Parameters
-localparam COUNT_WIDTH = $clog2(NUM_NA);
+localparam COUNT_WIDTH = $clog2(NUM_NA) + 1;
+localparam NUM_COMPARADOR = 8;
+
 genvar i;
+integer w;
 //Wires
 wire [CRITERIO_WIDTH-1:0] na_criterio_2d [0:NUM_NA-1];
 wire parar_contagem;
+wire [CRITERIO_WIDTH*NUM_COMPARADOR-1:0] comparador_data;
+reg [CRITERIO_WIDTH-1:0] comparador_data_2d [0:NUM_COMPARADOR-1];
+reg atualizar_comparador;
 //Registers
-reg [COUNT_WIDTH-1:0] count;
+reg [COUNT_WIDTH-1:0] count_comparador;
 
 //*******************************************************
 //General Purpose Signals
 //*******************************************************
-assign parar_contagem = count == NUM_NA-1;
+assign parar_contagem = count_comparador >= NUM_NA-1;
 
 //Convertendo entrada 1d para 2d
 generate
@@ -51,39 +57,47 @@ generate
     end
 endgenerate
 
+generate
+    for (i = 0; i < NUM_COMPARADOR; i = i + 1) begin:convert_dimension_comparador
+        assign comparador_data[CRITERIO_WIDTH*i+CRITERIO_WIDTH-1:CRITERIO_WIDTH*i] = comparador_data_2d[i];
+    end
+endgenerate
+
 always @(posedge clk or negedge rst_n) begin
-   if (!rst_n) begin
-      count <= {COUNT_WIDTH{1'b0}};
-   end
-   else begin
-   		if (aa_atualizar_in)
-         	count <= 1;
-   		else if (parar_contagem) begin
-     		count <= {COUNT_WIDTH{1'b0}};
-      	end
-      	else if (aa_atualizar_in || count != 0)
-      		count <= count + 1'b1;
-   end
+	if (!rst_n) begin
+		count_comparador <= {COUNT_WIDTH{1'b0}};
+		atualizar_comparador <= 1'b0;
+	end
+	else begin
+		atualizar_comparador <= 1'b0;
+		if (aa_atualizar_in)
+			count_comparador <= {COUNT_WIDTH{1'b0}};
+		else if (!parar_contagem) begin
+			atualizar_comparador <= 1'b1;
+			count_comparador <= count_comparador + NUM_COMPARADOR;
+		end
+	end
+end
+
+always @(posedge clk or negedge rst_n) begin
+	if (!rst_n) begin
+		for (w = 0; w < NUM_COMPARADOR; w = w + 1) begin
+        	comparador_data_2d[w] <= {CRITERIO_WIDTH{1'b1}};
+		end
+	end
+	else begin
+		for (w = 0; w < NUM_COMPARADOR; w = w + 1) begin
+			if ((count_comparador + w) >= NUM_NA)
+            	comparador_data_2d[w] <= {CRITERIO_WIDTH{1'b1}};
+            else
+            	comparador_data_2d[w] <= na_criterio_2d[count_comparador+w];
+		end
+	end
 end
 
 //*******************************************************
 //Outputs
 //*******************************************************
-// Otimizar essa lógica para paralelo
-always @(posedge clk or negedge rst_n) begin
-	if (!rst_n) begin
-		ca_criterio_geral_out <= {CRITERIO_WIDTH{1'b1}};
-	end
-	else begin			
-		if (aa_atualizar_in)
-			if (na_ativo_in[0])
-				ca_criterio_geral_out = na_criterio_2d[0];
-			else
-				ca_criterio_geral_out <= {CRITERIO_WIDTH{1'b1}};
-		else if ((ca_criterio_geral_out > na_criterio_2d[count]) & na_ativo_in[count])
-			ca_criterio_geral_out <= na_criterio_2d[count];
-	end
-end
 
 always @(posedge clk or negedge rst_n) begin
 	if (!rst_n) begin
@@ -99,5 +113,20 @@ end
 //*******************************************************
 //Instantiations
 //*******************************************************
+
+comparador_na
+	#(
+		.DATA_WIDTH(CRITERIO_WIDTH),
+		.NUM_COMPARADOR(NUM_COMPARADOR)
+	)
+	comparador_u
+	(/*autoport*/
+		.clk(clk),
+		.rst_n(rst_n),
+		.iniciar_in(aa_atualizar_in),
+		.atualizar_in(atualizar_comparador),
+		.data_in(comparador_data),
+		.data_out(ca_criterio_geral_out)
+	);
 
 endmodule
---
+++ fpga_proj/rtl/comparador_na.v
@@ -0,0 +1,57 @@
+//==================================================================================================
+//  Filename      : comparador_na.v
+//  Created On    : 2023-01-19 08:57:42
+//  Last Modified : 2023-01-19 11:12:12
+//  Revision      : 
+//  Author        : Linton Esteves
+//  Company       : UFBA
+//  Email         : lintonthiago@gmail.com
+//
+//  Description   : 
+//
+//
+//==================================================================================================
+module comparador_na
+		#(
+			DATA_WIDTH = 8,
+			NUM_COMPARADOR = 8
+		)
+		(/*autoport*/
+			input clk,
+			input rst_n,
+			input iniciar_in,
+			input atualizar_in,
+			input [DATA_WIDTH*NUM_COMPARADOR-1:0] data_in,
+			output reg [DATA_WIDTH-1:0] data_out
+		);
+
+integer w;
+genvar i;
+wire [DATA_WIDTH-1:0] data_in_2d [0: NUM_COMPARADOR-1];
+
+generate
+    for (i = 0; i < NUM_COMPARADOR; i = i + 1) begin:convert_dimension_in
+        assign data_in_2d[i] = data_in[DATA_WIDTH*i+DATA_WIDTH-1:DATA_WIDTH*i];
+    end
+endgenerate
+
+//*******************************************************
+//Outputs
+//*******************************************************
+always @(posedge clk or negedge rst_n) begin
+	if (!rst_n) begin
+		data_out <= {DATA_WIDTH{1'b1}};
+	end
+	else begin
+		if (iniciar_in)
+			data_out <= {DATA_WIDTH{1'b1}};
+		else begin
+			for (w = 0; w < NUM_COMPARADOR; w = w + 1) begin
+            	if (data_out > data_in_2d[w])
+					data_out <= data_in_2d[w];
+        	end
+		end
+	end
+end
+
+endmodule
